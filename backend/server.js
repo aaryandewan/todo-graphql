40ba7344f40f1ccb748e2d27af5fe52320ca99db
Aaryan Dewan <aaryan.dewan@iongroup.com>
2023-10-03 15:50:56 +0530

    server runs success

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const connectDB = require("./config/db"); // path to your db.js
+const { ApolloServer } = require("apollo-server-express");
+const connectDB = require("./config/db");
+const typeDefs = require("./graphql/typeDefs/index");
+const resolvers = require("./graphql/resolvers/todoResolvers");
 
 const app = express();
 const PORT = 4000;
 
-// Connect to the database
+// Connect to the MongoDB database
 connectDB();
+
+// Initialize Apollo Server
+let x;
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req }),
+  });
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+  console.log(
+    `GraphQL Playground available at http://localhost:${PORT}${server.graphqlPath}`
+  );
+}
+startServer();
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
+  // console.log(
+  //   `GraphQL Playground available at http://localhost:${PORT}${server.graphqlPath}`
+  // );
 });
